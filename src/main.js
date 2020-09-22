eaa731cbaa0ba5ae6968bb048ddcaf475db5133d
e-mbrown <ebsonari@gmail.com>
2020-09-21 21:06:43 -0400

    fixed link

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from "./router";
 import Buefy from "buefy";
 import "buefy/dist/buefy.css";
 
-Vue.prototype.$URL = 'https://p4-backendebs.herokuapp.com/'
+Vue.prototype.$URL = 'https://p4-backendebs.herokuapp.com'
 
 Vue.use(Buefy);
 
